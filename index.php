93c65445cc190871323393659e285a0e607ad6ee
met-mw <met-mw@rambler.ru>
2015-10-17 18:43:40 +0300

    Константы путей

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
  * Date: 03.10.15
  */
 
+define('SCMS_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
+define('SCMS_APPLICATION_ROOT', SCMS_ROOT . 'application' . DIRECTORY_SEPARATOR);
+define('SCMS_PUBLIC_ROOT', SCMS_ROOT . 'public' . DIRECTORY_SEPARATOR);
+
 
 include_once('bootstrap.php');
