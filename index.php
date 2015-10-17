8463d6e8e15f57be081a66ab0d9c5cc9177c9ea8
met-mw <met-mw@rambler.ru>
2015-10-17 18:53:25 +0300

    Константа публичной зоны

--- index.php
+++ index.php
@@ -9,5 +9,7 @@ define('SCMS_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
 define('SCMS_APPLICATION_ROOT', SCMS_ROOT . 'application' . DIRECTORY_SEPARATOR);
 define('SCMS_PUBLIC_ROOT', SCMS_ROOT . 'public' . DIRECTORY_SEPARATOR);
 
+define('SCMS_PUBLIC_HREF', DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR);
+
 
 include_once('bootstrap.php');
