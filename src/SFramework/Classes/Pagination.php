cc56ebd27964dc845674bed6fe7c56641fd13c95
met-mw <met-mw@rambler.ru>
2015-11-30 17:54:01 +0300

    Пагинация

---
+++ src/SFramework/Classes/Pagination.php
@@ -0,0 +1,114 @@
+<?php
+namespace SFramework\Classes;
+
+
+use SORM\Interfaces\InterfaceDriver;
+use SORM\Tools\Builder\Select;
+
+class Pagination {
+
+    /** @var int */
+    protected $fullCount = 0;
+    /** @var int|null  */
+    protected $limit = null;
+    /** @var int|null */
+    protected $linksLimit = null;
+    /** @var int|null */
+    protected $current = null;
+    /** @var int|null */
+    protected $pagesCount = null;
+    protected $parameterName = 'page';
+
+    /** @var Select */
+    protected $builder;
+    /** @var InterfaceDriver */
+    protected $driver;
+
+    public function __construct(InterfaceDriver $driver, $current = null, $limit = null, $parameterName = 'page') {
+        $this->driver = $driver;
+        $this->builder = new Select();
+        $this->parameterName = $parameterName;
+        $this->current = $current;
+        $this->limit = $limit;
+    }
+
+    /**
+     * @param int $linksLimit
+     */
+    public function setLinksLimit($linksLimit) {
+        $this->linksLimit = $linksLimit;
+    }
+
+    /**
+     * @param int $current Порядковый номер активной страницы
+     *
+     * @return $this
+     */
+    public function setCurrent($current) {
+        $this->current = $current;
+        return $this;
+    }
+
+    /**
+     * @param int $limit
+     *
+     * @return $this
+     */
+    public function setLimit($limit) {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function prepare() {
+        $this->calcFullRowsCount();
+        $this->calcPagesCount();
+
+        return $this;
+    }
+
+    public function getOffset() {
+        return (int)$this->current * $this->limit;
+    }
+
+    public function getLimit() {
+        return (int)$this->limit;
+    }
+
+    public function getUrl() {
+        parse_str($_SERVER['QUERY_STRING'], $queryVars );
+        if (isset($queryVars[$this->parameterName])) {
+            unset($queryVars[$this->parameterName]);
+        }
+
+        return $_SERVER['REDIRECT_URL'] . '?' . http_build_query($queryVars);
+    }
+
+    public function getPagesCount() {
+        return ceil($this->fullCount / $this->limit);
+    }
+
+    public function getParameterName() {
+        return $this->parameterName;
+    }
+
+    /**
+     * @return $this
+     */
+    protected function calcFullRowsCount() {
+        $builder = new Select();
+        $builder->field('FOUND_ROWS()', 'count');
+        $query = $builder->build();
+        $this->driver->query($query);
+        $this->fullCount = (int)reset($this->driver->fetchAssoc());
+
+        return $this;
+    }
+
+    protected function calcPagesCount() {
+        $this->pagesCount = ceil($this->fullCount / $this->limit);
+    }
+
+} 
---
+++ src/SFramework/Views/ViewPagination.php
@@ -0,0 +1,43 @@
+<?php
+namespace SFramework\Views;
+
+
+use SFramework\Classes\View;
+
+class ViewPagination extends View {
+
+    /** @var int */
+    public $pagesCount;
+    /** @var int */
+    public $currentPage;
+    /** @var string */
+    public $currentURL;
+    /** @var string */
+    public $parameterName;
+
+    public function currentRender() {
+        ?>
+        <ul>
+            <li>
+                <a href="<?= $this->currentURL ?>&<?= $this->parameterName ?>=<?= ($this->currentPage <= 1 ? 1 : $this->currentPage - 1) ?>">
+                    <span>&laquo;</span>
+                </a>
+            </li>
+            <? for ($i = 1; $i < $this->pagesCount; $i++): ?>
+                <li>
+                    <? if ($i == $this->currentPage): ?>
+                        <span><?= $this->currentPage ?></span>
+                    <? else: ?>
+                        <a href="<?= $this->currentURL ?>&<?= $this->parameterName ?>=<?= $i ?>"><?= $i ?></a>
+                    <? endif; ?>
+                </li>
+            <? endfor; ?>
+            <li>
+                <a href="<?= $this->currentURL ?>&<?= $this->parameterName ?>=<?= ($this->currentPage >= $this->pagesCount ? $this->pagesCount : $this->currentPage + 1) ?>">
+                    <span>&raquo;</span>
+                </a>
+            </li>
+        </ul>
+        <?
+    }
+}
