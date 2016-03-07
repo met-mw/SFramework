df9ffb7d55d6534a3a0ae1cc06f3cf4cb5bea764
met-mw <met-mw@rambler.ru>
2016-03-07 16:31:48 +0300

    Причесал ДатаГрид

--- src/SFramework/Classes/CoreFunctions.php
+++ src/SFramework/Classes/CoreFunctions.php
@@ -14,7 +14,11 @@ class CoreFunctions {
     }
 
     static public function addGETParamToURI($uri, $name, $value) {
-        $uri .= (mb_strpos($uri, '?') === false ? '?' : '&') . "{$name}={$value}";
+        if (mb_strpos($uri, '?') !== false && preg_match('/(test=[^&]*){1}/', $uri)) {
+            $uri = preg_replace('/(test=[^&]*){1}/', "{$name}={$value}", $uri);
+        } else {
+            $uri .= (mb_strpos($uri, '?') === false ? '?' : '&') . "{$name}={$value}";
+        }
 
         return $uri;
     }
