ad8a05abd98105269ea2eb6fc661ce4c8b26e6d3
met-mw <met-mw@rambler.ru>
2016-03-12 00:11:43 +0300

    DataGrid - декоратор, ссылка

--- src/SFramework/Views/DataGrid/ViewLink.php
+++ src/SFramework/Views/DataGrid/ViewLink.php
@@ -4,8 +4,15 @@ namespace SFramework\Views\DataGrid;
 
 class ViewLink extends ViewDecoration {
 
+    /** @var string Формат: /path/to/{label}/ */
+    public $urlTemplate;
+
+    public function __construct($urlTemplate) {
+        $this->urlTemplate = $urlTemplate;
+    }
+
     public function currentRender() {
-        ?><a target="_blank" href="<?= $this->getValue() ?>"><?= $this->getValue() ?></a><?
+        ?><a target="_blank" href="<?= str_replace('{label}', $this->getValue(), $this->urlTemplate) ?>"><?= $this->getValue() ?></a><?
     }
 
 }
