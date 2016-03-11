a006e88598044130304f55e1fc966212861102f1
met-mw <met-mw@rambler.ru>
2016-03-12 00:40:10 +0300

    DataGrid - декоратор, опциональное поле

--- src/SFramework/Views/DataGrid/ViewEmpty.php
+++ src/SFramework/Views/DataGrid/ViewEmpty.php
@@ -6,11 +6,19 @@ namespace SFramework\Views\DataGrid;
 
 class ViewEmpty extends ViewDecoration {
 
+    public $isEmptyText;
+    public $isNotEmptyText;
+
+    public function __construct($isEmptyText = ' - ', $isNotEmptyText = ' + ') {
+        $this->isEmptyText = $isEmptyText;
+        $this->isNotEmptyText = $isNotEmptyText;
+    }
+
     public function currentRender() {
         $isEmpty = is_null($this->getValue()) || $this->getValue() === '';
         ?>
         <span class="label label-<?= $isEmpty ? 'default' : 'success' ?>">
-            <?= $isEmpty ? '---' : $this->getValue() ?>
+            <?= $isEmpty ? $this->isEmptyText : $this->isNotEmptyText ?>
         </span>
         <?
     }
