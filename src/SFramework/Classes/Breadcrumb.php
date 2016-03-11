21fb8bc3e1a9c297d2ecc6cca0ba02536bf01ff5
met-mw <met-mw@rambler.ru>
2016-03-12 01:42:29 +0300

    Хлебные крошки, тотальное упрощение

--- src/SFramework/Classes/Breadcrumb.php
+++ src/SFramework/Classes/Breadcrumb.php
@@ -4,12 +4,14 @@ namespace SFramework\Classes;
 
 class Breadcrumb {
 
-    public $name;
-    public $path;
+    protected $name;
+    protected $path;
+    protected $isParam;
 
-    public function __construct($name, $path) {
-        $this->name = $name;
-        $this->path = $path;
+    public function __construct($name, $path, $isParam = false) {
+        $this->setName($name)
+            ->setPath($path)
+            ->setIsParam($isParam);
     }
 
     public function getName() {
@@ -20,14 +22,38 @@ class Breadcrumb {
         return $this->path;
     }
 
+    /**
+     * @param $name
+     *
+     * @return Breadcrumb
+     */
     public function setName($name) {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * @param $path
+     *
+     * @return Breadcrumb
+     */
     public function setPath($path) {
         $this->path = $path;
-        return $path;
+        return $this;
+    }
+
+    /**
+     * @param $isParam
+     *
+     * @return Breadcrumb
+     */
+    public function setIsParam($isParam) {
+        $this->isParam = $isParam;
+        return $this;
+    }
+
+    public function isParam() {
+        return $this->isParam;
     }
 
 } 
