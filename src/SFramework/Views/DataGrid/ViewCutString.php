6a645c0a6c94c72a740929e0f13394b47b3a5963
met-mw <met-mw@rambler.ru>
2016-03-15 01:03:48 +0300

    DataGrid. Декоратор - обрезанная строка

---
+++ src/SFramework/Views/DataGrid/ViewCutString.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace SFramework\Views\DataGrid;
+
+
+use SFramework\Classes\CoreFunctions;
+
+class ViewModal extends ViewDecoration {
+
+    /** @var integer */
+    public $offset;
+    /** @var bool */
+    public $stripTags;
+    /** @var array */
+    public $attributesLink = [];
+    /** @var array */
+    public $attributesFullContent = [];
+
+    public function __construct($offset, $stripTags = true, array $attributesLink = [], array $attributesFullContent = []) {
+        $this->attributesLink = $attributesLink;
+        $this->attributesFullContent = $attributesFullContent;
+        $this->offset = $offset;
+        $this->stripTags = $stripTags;
+    }
+
+    public function currentRender() {
+        $content = $this->stripTags ? strip_tags($this->getValue()) : $this->getValue();
+        $content = mb_substr($content, mb_strpos($content, ' ', $this->offset));
+        ?><a<?= !empty($this->attributesLink) ? CoreFunctions::tagAttributesToString($this->attributesLink) : '' ?>><?= $content ?>...</a><?
+        ?><div<?= !empty($this->attributesFullContent) ? CoreFunctions::tagAttributesToString($this->attributesFullContent) : '' ?>><?= $this->getValue() ?></div><?
+    }
+
+}
