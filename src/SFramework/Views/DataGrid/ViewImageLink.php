c49460d40ee84779ce018ede516b98887b4f7e62
met-mw <met-mw@rambler.ru>
2016-03-15 12:14:11 +0300

    Датагрид. Декоратор. Кликабельная картинка

--- src/SFramework/Views/DataGrid/ViewImageLink.php
+++ src/SFramework/Views/DataGrid/ViewImageLink.php
@@ -8,10 +8,6 @@ use SFramework\Classes\CoreFunctions;
 
 class ViewImageLink extends ViewDecoration {
 
-    /** @var string Формат: /path/to/{label}/ */
-    public $urlTemplate;
-    /** @var string|null */
-    public $valueFieldName;
     /** @var bool */
     public $blank;
     /** @var array */
@@ -21,22 +17,14 @@ class ViewImageLink extends ViewDecoration {
     public function __construct($urlTemplate, $blank = false, $valueFieldName = null, array $attributesLink = [], array $attributesImage = []) {
         $this->optional[] = 'valueFieldName';
 
-        $this->urlTemplate = $urlTemplate;
         $this->blank = $blank;
-        $this->valueFieldName = $valueFieldName;
         $this->attributesLink = $attributesLink;
         $this->attributesImage = $attributesImage;
     }
 
     public function currentRender() {
-        $additionalData = $this->getAdditionalData();
-        $label = $this->getValue();
-        if ($this->valueFieldName !== null && isset($additionalData[$this->valueFieldName])) {
-            $label = $additionalData[$this->valueFieldName];
-        }
-
         ?>
-        <a<?= !empty($this->attributesLink) ? ' ' . CoreFunctions::tagAttributesToString($this->attributesLink) : '' ?><?= $this->blank ? ' target="_blank"' : '' ?> href="<?= str_replace('{label}', $label, $this->urlTemplate) ?>"><?= $this->getValue() ?>
+        <a<?= !empty($this->attributesLink) ? ' ' . CoreFunctions::tagAttributesToString($this->attributesLink) : '' ?><?= $this->blank ? ' target="_blank"' : '' ?> href="<?= $this->getValue() ?>">
             <img<?= !empty($this->attributesImage) ? ' ' . CoreFunctions::tagAttributesToString($this->attributesImage) : '' ?> src="<?= $this->getValue() ?>"/>
         </a>
         <?
