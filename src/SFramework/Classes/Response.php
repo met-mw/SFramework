c7d8557c3530d315af5e8a29606f3d87002d29ca
met-mw <met-mw@rambler.ru>
2016-05-30 13:35:46 +0300

    Доработал Response для работы без ajax

--- src/SFramework/Classes/Response.php
+++ src/SFramework/Classes/Response.php
@@ -25,9 +25,7 @@ class Response
 
     public function send($redirect = '', array $additionalData = [])
     {
-        $success = !($this->NotificationLog->hasErrors()
-            || $this->NotificationLog->hasWarnings()
-            || $this->NotificationLog->hasNotices());
+        $success = !$this->NotificationLog->hasProblems();
 
         $errors = [];
         $warnings = [];
@@ -52,17 +50,42 @@ class Response
             $messages[] = $message;
         }
 
-        echo $this->arrayToJSON(
-            [
-                'success' => $success,
-                'errors' => $errors,
-                'warnings' => $warnings,
-                'notices' => $notices,
-                'messages' => $messages,
-                'redirect' => $redirect,
-                'additional_data' => $additionalData
-            ]
-        );
+        if (CoreFunctions::isAJAX()) {
+            echo $this->arrayToJSON(
+                [
+                    'success' => $success,
+                    'errors' => $errors,
+                    'warnings' => $warnings,
+                    'notices' => $notices,
+                    'messages' => $messages,
+                    'redirect' => $redirect,
+                    'additional_data' => $additionalData
+                ]
+            );
+        } else {
+            if ($redirect) {
+                header("Location:{$redirect}");
+            } else {
+                echo '<h3>Системные сообщения:</h3>';
+                echo 'Отчёт о выполнении: ', $success ? 'Успешно' : 'Возникли проблемы', '<hr/>';
+                if (!empty($errors)) {
+                    echo 'Ошибки:<br/><ul>', '<li>', implode('</li>', $errors), '</li>', '</ul>';
+                }
+                if (!empty($warnings)) {
+                    echo 'Предупреждения:<br/><ul>', '<li>', implode('</li>', $warnings), '</li>', '</ul>';
+                }
+                if (!empty($notices)) {
+                    echo 'Уведомления:<br/><ul>', '<li>', implode('</li>', $notices), '</li>', '</ul>';
+                }
+                if (!empty($messages)) {
+                    echo 'Сообщения:<br/><ul>', '<li>', implode('</li>', $messages), '</li>', '</ul>';
+                }
+                if (!empty($additionalData)) {
+                    echo '<hr/><h4>Дополнительные данные:</h4>', var_export($additionalData);
+                }
+            }
+        }
+
     }
 
 } 
